84cdfb7fac581f768a64ce5b0e3ef0856681347b
seongmin2223 <khyuno9@naver.com>
2025-10-08 14:59:58 +0900

    first commit

---
+++ src/main/java/org/example/blogcrud/BlogCrudApplication.java
@@ -0,0 +1,13 @@
+package org.example.blogcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/blogcrud/BlogCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.blogcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
